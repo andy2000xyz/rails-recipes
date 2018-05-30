f3de04f6f0e0d8c887bdf566a73254be4a08e033
andy2000xyz <andy2000xyz@hotmail.com>
2018-05-30 21:26:45 +0800

    实做Bulk Editing批次编辑

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -46,6 +46,26 @@ class Admin::EventsController < AdminController
     redirect_to admin_events_path
   end
 
+  def bulk_update
+    total = 0
+    Array(params[:ids]).each do |event_id|
+      event = Event.find(event_id)
+
+      if params[:commit] == I18n.t(:bulk_update)
+        event.status = params[:event_status]
+        if event.save
+          total = 1
+        end
+      elsif params[:commit] == I18n.t(:bulk_delete)
+        event.destroy
+        total = 1
+      end
+    end
+
+    flash[:alert] = "成功完成 #{total} 笔"
+    redirect_to admin_events_path
+  end
+
   protected
 
   def event_params
@@ -54,3 +74,5 @@ class Admin::EventsController < AdminController
 
 end
 # 这里 @event.tickets.build 两次，等会表单中就有两笔空的 Ticket 可以编辑。Strong Parameters 的部分，新增了 tickets_attributes 数组包含要修改的 ticket 属性，并且额外多了一个 :id 和 :_destroy 是为了配合 accepts_nested_attributes_for 可以编辑和删除。
+
+# Pro Tip 小技巧：这里 ihower 老师改用了 I18n 来显示按钮字串，这倒不是因为一定要支援多语言，而是因为文案可能会改。如果你按钮写死 <%= submit_tag '批次修改' 的话，那么在 controller 中也需要写成 if params[:commit] == '批次修改'。将来那一天要改字，就要记得两个地方都要改到。但是如果用 I18n 来处理，就之后只要记得改翻译档一个地方就好了。这个原则就叫做 DRY: Don't repeat yourself，这是一个写好程序的基本原则。
