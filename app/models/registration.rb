b8a65752dd79d491b4e98a8ccc3291d24f536ffe
andy2000xyz <andy2000xyz@hotmail.com>
2018-05-31 07:40:07 +0800

    实做多步骤表单上一步-下一步-有条件表单验证功能

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,7 +2,10 @@ class Registration < ApplicationRecord
   STATUS = ["pending", "confirmed"]
   validates_inclusion_of :status, :in => STATUS
   validates_presence_of :status, :ticket_id
-
+  attr_accessor :current_step
+    validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
+    validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
+  # Rails 可以根据条件来做表单验证，叫做 Conditional Validations。我们需要根据用户实际在做哪一步，来决定要启用哪些验证。
   belongs_to :event
   belongs_to :ticket
   belongs_to :user, :optional => true
@@ -14,6 +17,14 @@ class Registration < ApplicationRecord
   end
 
   protected
+
+  def should_validate_basic_data?
+    current_step == 2  # 只有做到第二步需要验证
+  end
+
+  def should_validate_all_data?
+    current_step == 3 || status == "confirmed"  # 做到第三步，或最后状态是 confirmed 时需要验证
+  end
 
   def generate_uuid
     self.uuid = SecureRandom.uuid
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,18 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :events do
-    resources :registrations
+    resources :registrations do
+      member do
+        get "steps/1" => "registrations#step1", :as => :step1
+        patch "steps/1/update" => "registrations#step1_update", :as => :update_step1
+        get "steps/2" => "registrations#step2", :as => :step2
+        patch "steps/2/update" => "registrations#step2_update", :as => :update_step2
+        get "steps/3" => "registrations#step3", :as => :step3
+        patch "steps/3/update" => "registrations#step3_update", :as => :update_step3
+      end
+    end
   end
-
+  # 没有特别看到 step1 是因为 registrations controller 本来的 new action 和 create action 就是新增时的第一步。
   resource :user
 
   namespace :admin do
@@ -25,8 +34,6 @@ Rails.application.routes.draw do
       resource :profile, :controller => "user_profiles"
     end
   end
-
-
 
   get "/faq" => "pages#faq"
 
