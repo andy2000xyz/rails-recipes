e133b16a221e1112424232060f8855ec26a711c4
andy2000xyz <andy2000xyz@hotmail.com>
2018-05-31 22:05:22 +0800

    上传多档案(使用额外Model)

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,13 +2,16 @@ class Event < ApplicationRecord
   mount_uploader :logo, EventLogoUploader
   mount_uploaders :images, EventImageUploader
   serialize :images, JSON
-  
+
   include RankedModel
   ranks :row_order
   belongs_to :category, :optional => true
   has_many :tickets, :dependent => :destroy, :inverse_of  => :event
   has_many :registrations, :dependent => :destroy
     # 某些版本的Rails 有个accepts_nested_attributes_for 的bug 让has_many 故障了，需要额外补上inverse_of 参数，不然存储时会找不到tickets
+
+  has_many :attachments, :class_name => "EventAttachment", :dependent => :destroy
+  accepts_nested_attributes_for :attachments, :allow_destroy => true, :reject_if => :all_blank
   accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
 
   STATUS = ["draft", "public", "private"]
