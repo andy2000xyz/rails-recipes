16dbaa4e310d063a4c7614f7d14e086f6a54082a
andy2000xyz <andy2000xyz@hotmail.com>
2018-05-31 14:08:01 +0800

    制作后台管理报名资料

---
+++ app/controllers/admin/event_registrations_controller.rb
@@ -0,0 +1,45 @@
+class Admin::EventRegistrationsController < ApplicationController
+  before_action :find_event
+
+  def index
+    @registrations = @event.registrations.includes(:ticket).order("id DESC")
+  end
+
+  def new
+    @registration = @event.registrations.new
+  end
+
+  def create
+    @registration = @event.registrations.new(registration_params)
+    if @registration.save
+      redirect_to admin_event_registrations_path(@event)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @registration = @event.registrations.find(params[:id])
+  end
+
+  def update
+    @registration = @event.registrations.find(params[:id])
+  end
+
+  def destroy
+    @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.destroy
+
+    redirect_to admin_event_registrations_path(@event)
+  end
+
+  protected
+
+  def find_event
+    @event = Event.find_by_friendly_id!(params[:event_id])
+  end
+
+  def registration_params
+    params.require(:registration).permit(:status, :ticket_id, :name, :email, :cellphone, :website, :bio)
+  end
+end
