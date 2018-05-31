f29f4d341327f3cbae6134ab9fa2c8198ff026d3
andy2000xyz <andy2000xyz@hotmail.com>
2018-05-31 15:53:38 +0800

    时间区间筛选

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -4,13 +4,13 @@ class Admin::EventRegistrationsController < ApplicationController
   def index
     @registrations = @event.registrations.includes(:ticket).order("id DESC").page(params[:page])
 
-    # if params[:status].present? && Registration::STATUS.include?(params[:status])
-    #   @registrations = @registrations.by_status(params[:status])
-    # end
-    #
-    # if params[:ticket_id].present?
-    #   @registrations = @registrations.by_ticket(params[:ticket_id])
-    # end
+    if params[:start_on].present?
+      @registrations = @registrations.where( "created_at >= ?", Date.parse(params[:start_on]).beginning_of_day )
+    end
+
+    if params[:end_on].present?
+      @registrations = @registrations.where( "created_at <= ?", Date.parse(params[:end_on]).end_of_day )
+    end
 
     if Array(params[:statuses]).any?
       @registrations = @registrations.by_status(params[:statuses])
