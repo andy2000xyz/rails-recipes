8467e5389ce2b97154a3e42beea8b2a6552a1534
andy2000xyz <andy2000xyz@hotmail.com>
2018-05-31 09:21:20 +0800

    内联式（inline）错误讯息和自定义资料验证错误显示

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -6,6 +6,8 @@ class Registration < ApplicationRecord
     validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
     validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
   # Rails 可以根据条件来做表单验证，叫做 Conditional Validations。我们需要根据用户实际在做哪一步，来决定要启用哪些验证。
+  validate :check_event_status, :on => :create
+  
   belongs_to :event
   belongs_to :ticket
   belongs_to :user, :optional => true
@@ -17,6 +19,16 @@ class Registration < ApplicationRecord
   end
 
   protected
+
+  def check_event_status
+    if self.event.status == "draft"
+      errors.add(:base, "活动尚未开放报名")
+    end
+  end
+  # 解说：
+  # *  validate 可以增加自定义的资料验证，后面的 :on => :create 参数可以指定只有新建才会验证(默认是新建跟修改都会验证)
+  # * 验证不通过时，会用errors.add 增加错误讯息，第一个参数是字段名称，第二个参数是错误讯息
+  # * 因为表单上并没有 event_id 这个输入框，所以就算写成 errors.add(:event_id, "活动尚未开放报名") 也没有地方显示出来。依照惯例，任何不属于某个字段的错误，我们会放在 :base 上。
 
   def should_validate_basic_data?
     current_step == 2  # 只有做到第二步需要验证
